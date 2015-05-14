f7bb5df11249f8c5375f0cd52a3aad0a8e7e3ca8
Simon Prickett <simon@crudworks.org>
2015-05-14 12:49:18 -0400

    Added first cut get distance function.

--- server.js
+++ server.js
@@ -43,6 +43,19 @@ loadStationList = function() {
 	});
 };
 
+getDistance = function(latUser, lonUser, latStation, lonStation) {
+	var R = 6371;
+	var dLat = (latStation - latUser).toRad();
+	var dLon = (lonStation - lonUser).toRad();
+	var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        	Math.cos(latStation.toRad()) * Math.cos(latUser.toRad()) *
+        	Math.sin(dLon / 2) * Math.sin(dLon / 2);
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	var d = R * c;
+	
+	return d;
+};
+
 router.route('/').get(
 	function(request, response) {
 		response.sendFile('index.html', { root: path.join(__dirname, './html') });
